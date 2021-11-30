2112b25964829339157858561f32087674c65f94
Dreamwings <huyb2009@gmail.com>
2021-11-29 18:51:16 -0800

    Time: 184 ms (74.53%), Space: 17.2 MB (31.85%) - LeetHub

---
+++ minesweeper/minesweeper.py
@@ -0,0 +1,35 @@
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        
+        m, n = len(board), len(board[0])
+        a, b = click
+        
+        if board[a][b] == 'M':
+            board[a][b] = 'X'
+            return board
+        
+        def dfs(x, y):
+            d = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+            mines = 0
+            
+            for dx, dy in d:
+                i, j = x + dx, y + dy
+                if 0 <= i < m and 0 <= j < n and board[i][j] == 'M':
+                    mines += 1
+            
+            if mines == 0:
+                board[x][y] = 'B'
+            else:
+                board[x][y] = str(mines)
+                return
+            
+            for dx, dy in d:
+                i, j = x + dx, y + dy
+                if 0 <= i < m and 0 <= j < n and board[i][j] == 'E':
+                    dfs(i, j)
+                    
+    
+        dfs(a, b)
+        return board
+    
+    
