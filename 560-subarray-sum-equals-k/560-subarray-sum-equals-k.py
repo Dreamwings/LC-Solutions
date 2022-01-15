3574b423437d75fbccc704e6ce46a9002cb28f04
Dreamwings <huyb2009@gmail.com>
2022-01-15 12:26:40 -0800

    Time: 212 ms (84.44%), Space: 16.2 MB (28.96%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        from collections import defaultdict
+        
+        ## S1:
+        
+        pre = defaultdict(int)  # store the number of each presum value
+        pre[0] = 1
+        s, res = 0, 0
+        
+        for x in nums:
+            s += x
+            if s - k in pre:
+                res += pre[s - k]
+            pre[s] += 1
+        
+        return res
+        
+        """
+        pre = defaultdict(int)  # store the number of each presum value
+        s, res = 0, 0
+        
+        for x in nums:
+            s += x
+            if s == k:
+                res += 1
+            if s - k in pre:
+                res += pre[s - k]
+            pre[s] += 1
+        
+        return res
+        
+        
+        """
