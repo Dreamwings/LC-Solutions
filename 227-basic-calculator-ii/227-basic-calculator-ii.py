22f3b4cef6354e22b9927fb7cea48f1081dcce9b
Dreamwings <huyb2009@gmail.com>
2022-01-16 11:51:46 -0800

    Time: 120 ms (34.06%), Space: 15.9 MB (35.90%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+        stack = []
+        v = 0
+        pre_op = '+'
+        s += '+'
+        
+        for c in s:
+            if c in '0123456789':
+                v = 10 * v + int(c)
+            elif c in '+-*/':
+                if pre_op == '+':
+                    stack.append(v)
+                if pre_op == '-':
+                    stack.append(-v)
+                if pre_op == '*':
+                    x = stack.pop()
+                    stack.append(x * v)
+                if pre_op == '/':
+                    x = stack.pop()
+                    stack.append(int(x/v))
+                v, pre_op = 0, c
+                
+        return sum(stack)
