a7d73454d7ef52b95e30bf8957ef417aeb225708
Dreamwings <huyb2009@gmail.com>
2022-01-17 15:44:29 -0800

    Time: 40 ms (88.01%), Space: 14.4 MB (49.10%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        n = days[-1]
+        dp = [0] * (n + 1)
+        days = set(days)
+        
+        for i in range(1, n + 1):
+            if i not in days:
+                dp[i] = dp[i-1]
+            else:
+                i1 = max(0, i - 1)
+                i7 = max(0, i - 7)
+                i30 = max(0, i - 30)
+                dp[i] = min(dp[i1] + costs[0],
+                            dp[i7] + costs[1],
+                            dp[i30] + costs[2])
+        
+        return dp[-1]
