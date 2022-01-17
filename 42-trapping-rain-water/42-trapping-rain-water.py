58e0b891836dd10a373f99422bc3e3606c4e97e3
Dreamwings <huyb2009@gmail.com>
2022-01-16 23:39:07 -0800

    Time: 134 ms (5.24%), Space: 15 MB (16.71%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        ## Solution 1: Two Pointers
+        n = len(height)
+        if n <= 2: return 0
+        
+        i, j = 0, n - 1
+        l, r = height[0], height[n-1]  # max h from left and from right so far
+        res = 0
+        
+        while i < j:
+            h = min(l, r)
+            if l == h: # when l <= r
+                res += max(0, h - height[i])
+                i += 1
+                l = max(l, height[i])
+            else:  # when r == h, l > r
+                res += max(0, h - height[j])
+                j -= 1
+                r = max(r, height[j])
+                
+        return res
+            
+        
+        
