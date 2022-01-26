34fe2fbe978086475c21ba08a54c29435f254aa8
Dreamwings <huyb2009@gmail.com>
2022-01-25 21:28:53 -0800

    Time: 152 ms (99.02%), Space: 18.9 MB (28.28%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,33 +1,32 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         
+        
+        
         n = len(matrix)
         
-        def check(mid):
-            i, j = n - 1, 0
+        # define a function to find if the number of element on the left of mid is larger or equal than k
+        def check(mid): 
+            i, j = n - 1, 0  # starting from matrix[n-1][0]
             cnt = 0
-            
             while i >= 0 and j < n:
                 if matrix[i][j] <= mid:
-                    cnt += (i + 1)      # IMPORTANT!!!
-                    j += 1
+                    cnt += i + 1 # ***** IMPORTANT!!! *****
+                    # all num from matrix[0][j] to matrix[i][j] in column j is not larger than mid
+                    j += 1       # can move to the right of the same row
                 else:
-                    i -= 1
-            
+                    i -= 1       # can move to the up row, but the same column
+            # now we got the total number of vals on the left area of the line mid
             return cnt >= k
         
-        l, r = matrix[0][0], matrix[-1][-1]
-        
-        while l <= r:
-            m = (l + r) >> 1
-            # print(l, r, m, check(m))
-            if check(m):
-                r = m - 1
+        # now we can use binary search with check function
+        lo, hi = matrix[0][0], matrix[-1][-1]
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if check(mid):  # on the left of mid it has too many values
+                hi = mid - 1
             else:
-                l = m + 1
-        
-        return l
-        
-        
-        
-        
+                lo = mid + 1
+                
+        return lo
+            
