309fcbd30958fd030950b1e8d3be5f93876a0881
Dreamwings <huyb2009@gmail.com>
2022-01-28 15:05:04 -0800

    Time: 20 ms (58.10%), Space: 13.5 MB (34.76%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.py
@@ -0,0 +1,40 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n == 1: return 1
+        l, r = 1, n
+        
+        while l <= r:
+            m = (l + r) // 2
+            if isBadVersion(m) == True:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return l
+        
+        """
+        if n == 1: return 1
+        l, r = 1, n
+        
+        while l <= r:
+            m = (l + r) >> 1
+            if isBadVersion(m) == True and isBadVersion(m-1) == False:
+                return m
+            elif isBadVersion(m) == True:
+                r = m - 1
+            elif isBadVersion(m) == False:
+                l = m + 1
+        
+        """        
+        
+        
