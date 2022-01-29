8d970d9b96832bc01b348322c5781f516056c33d
Dreamwings <huyb2009@gmail.com>
2022-01-29 13:57:41 -0800

    Time: 1391 ms (5.03%), Space: 14.8 MB (94.17%) - LeetHub

---
+++ 661-image-smoother/661-image-smoother.py
@@ -0,0 +1,20 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        
+        m, n = len(img), len(img[0])
+        res = [[0] * n for _ in range(m)]
+        
+        def smoother(i, j):
+            s, cnt = 0, 0
+            for x in (i-1, i, i+1):
+                for y in (j-1, j, j+1):
+                    if 0 <= x < m and 0 <= y < n:
+                        cnt += 1
+                        s += img[x][y]
+            return s // cnt
+        
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = smoother(i, j)
+        
+        return res
