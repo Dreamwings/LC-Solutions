4718978dcd39193c14fe75814f10a1e2eb336899
Dreamwings <huyb2009@gmail.com>
2022-01-29 13:15:12 -0800

    Time: 135 ms (49.25%), Space: 15.4 MB (58.50%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,28 +1,29 @@
-class Solution:
-    def calculate(self, s: str) -> int:
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         
         res, v, sign = 0, 0, 1
-        stack = []
+        stack = [] # used when there is '()'
         
         for c in s:
             if c.isdigit():
                 v = 10 * v + int(c)
-            elif c == '+' or c == '-':
-                res += v * sign
+            elif c in '+-':
+                res += sign * v
                 v = 0
-                # sign = 1 if c == '+' else -1
-                sign = [-1, 1][c == '+']
+                sign = [-1, 1][c == '+'] # note first one is False, second one is True
             elif c == '(':
                 stack.append(res)
                 stack.append(sign)
                 res, sign = 0, 1
             elif c == ')':
                 res += sign * v
-                res *= stack.pop() # pop out the prev sign before '('
-                res += stack.pop() # pop out the prev res before '('
+                res *= stack.pop() # the sign before '('
+                res += stack.pop() # prev res before '('
                 v = 0
         
-        res += v * sign
+        res += sign * v
         return res
-        
-        
