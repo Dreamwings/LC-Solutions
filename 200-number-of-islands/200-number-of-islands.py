34bc64ed9cf5294fb918002b56dfb970e1ed0884
Dreamwings <huyb2009@gmail.com>
2022-01-29 12:44:16 -0800

    Time: 357 ms (50.19%), Space: 28.9 MB (42.99%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,7 +1,12 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
         
-        ## S1: DFS
+        
+        ## Solution 1: DFS
         
         m, n = len(grid), len(grid[0])
         dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
@@ -23,3 +28,39 @@ class Solution:
                     res += 1
                     
         return res
+        
+        """
+        ## Solution 2: BFS
+        
+        from collections import deque
+        
+        def bfs(x, y):
+            q = deque()
+            q.append((x, y))
+            grid[x][y] = '0'
+            d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+            
+            while q:
+                X, Y = q.popleft()
+                for dx, dy in d:
+                    i, j = X + dx, Y + dy
+                    if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                        q.append((i, j))
+                        grid[i][j] = '0'
+                        
+            return
+            
+        
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    bfs(i, j)
+                    res += 1
+        
+        return res
+        
+        
+        """
+        
+        
