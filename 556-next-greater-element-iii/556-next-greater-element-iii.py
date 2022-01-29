9fcb7f7a31c0e1b4cbe44bf95307a77d655f9cbe
Dreamwings <huyb2009@gmail.com>
2022-01-29 12:23:52 -0800

    Time: 15 ms (82.01%), Space: 13.3 MB (71.22%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -1,31 +1,33 @@
-class Solution:
-    def nextGreaterElement(self, n: int) -> int:
+class Solution(object):
+    def nextGreaterElement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        ## Ex: 123554321
+        ## ==> 124553321
+        ## ==> 124123355
         
         a = [int(x) for x in str(n)]
         m = len(a)
+        if m == 1: return -1
         i = j = m - 1
         
-        # find the first place a[i-1] < a[i] from right most
         while i and a[i-1] >= a[i]:
             i -= 1
         # consider case: 54321
-        if i == 0: 
-            return -1
+        if i == 0: return -1
         
-        # find the place to switch with a[i-1]
         k = i - 1
-        
         while j and a[j] <= a[k]:
             j -= 1
         
-        # now swap the digit at k and j
         a[k], a[j] = a[j], a[k]
-        # print(a)
-        # now need to reverse a[i:]
-        a[i:] = a[i:][::-1]
-        a = [str(x) for x in a]
-        res = int(''.join(a))
+        a[i:] = a[i:][::-1] # reverse subarray a[k+1:], note i = k + 1
         
+        res = int(''.join([str(x) for x in a]))
         if res <= 2**31 - 1: return res
-        else: return -1
+        return -1
+        
         
