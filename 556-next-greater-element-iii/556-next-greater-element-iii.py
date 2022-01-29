e9c0c58dfaa909f369394c9f15f83ed9026ad53f
Dreamwings <huyb2009@gmail.com>
2022-01-29 12:18:27 -0800

    Time: 32 ms (69.63%), Space: 13.8 MB (99.27%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -5,24 +5,27 @@ class Solution:
         m = len(a)
         i = j = m - 1
         
+        # find the first place a[i-1] < a[i] from right most
         while i and a[i-1] >= a[i]:
             i -= 1
-        
-        if i == 0:
+        # consider case: 54321
+        if i == 0: 
             return -1
         
+        # find the place to switch with a[i-1]
         k = i - 1
+        
         while j and a[j] <= a[k]:
             j -= 1
         
+        # now swap the digit at k and j
         a[k], a[j] = a[j], a[k]
-        
-        # now reverse a[i:]
+        # print(a)
+        # now need to reverse a[i:]
         a[i:] = a[i:][::-1]
-        
-        res = int(''.join([str(x) for x in a]))
+        a = [str(x) for x in a]
+        res = int(''.join(a))
         
         if res <= 2**31 - 1: return res
         else: return -1
-        
         
