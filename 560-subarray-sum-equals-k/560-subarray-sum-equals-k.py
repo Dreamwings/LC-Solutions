4bf27b3d443646fd4e1263bc05c74bf4a7e21106
Dreamwings <huyb2009@gmail.com>
2022-01-30 13:25:20 -0800

    Time: 216 ms (82.56%), Space: 16.1 MB (28.76%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -10,16 +10,16 @@ class Solution(object):
         
         ## S1:
         
-        pre = defaultdict(int)  # store the number of each presum value
-        pre[0] = 1
+        d = defaultdict(int) # store the number of each presum value
+        d[0] = 1  # IMPORTANT!!! presum == 0 before it starts at 0
         s, res = 0, 0
         
         for x in nums:
-            s += x
-            if s - k in pre:
-                res += pre[s - k]
-            pre[s] += 1
-        
+            s += x  # s is the cur presume
+            if s - k in d:
+                res += d[s - k]
+            d[s] += 1
+            
         return res
         
         """
