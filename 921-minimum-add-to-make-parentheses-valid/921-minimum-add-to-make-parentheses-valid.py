3498757629ccd955ac8c0f7e23b98311e4bd27c9
Dreamwings <huyb2009@gmail.com>
2022-01-30 15:37:25 -0800

    Time: 47 ms (30.66%), Space: 14 MB (87.28%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -1,6 +1,22 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         
+        ## S2: 
+        
+        missing_l, missing_r = 0, 0
+        
+        for c in s:
+            if c == '(':
+                missing_r += 1
+            elif c == ')':
+                if missing_r > 0:
+                    missing_r -= 1
+                else:
+                    missing_l += 1
+        
+        return missing_l + missing_r
+        
+        """
         ## S1:
         
         cnt, res = 0, 0
@@ -18,18 +34,4 @@ class Solution:
         
         
         """
-        ## S2: 
-        missing_l, missing_r = 0, 0
         
-        for c in s:
-            if c == '(':
-                missing_r += 1
-            elif c == ')':
-                if missing_r > 0:
-                    missing_r -= 1
-                else:
-                    missing_l += 1
-        
-        return missing_l + missing_r
-        
-        """
