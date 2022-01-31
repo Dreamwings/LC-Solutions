daae0e2a784aa7418b443d72d3ce0982453999a9
Dreamwings <huyb2009@gmail.com>
2022-01-31 12:01:29 -0800

    Time: 1096 ms (30.98%), Space: 17.1 MB (98.91%) - LeetHub

---
+++ 525-contiguous-array/525-contiguous-array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findMaxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        ## S1:
+        
+        n = len(nums)
+        d = {0: -1}
+        cnt = res = 0
+        
+        for i, x in enumerate(nums):
+            cnt += [-1, 1][x == 1]  # x: 1, cnt += 1; 0, cnt -= 1
+            # cnt is the number difference between 1 and 0
+            # if we find the same value of cnt previously at j, it means:
+            # nums[:j+1] and nums[:i+1] has the same cnt value
+            if cnt in d:
+                j = d[cnt]
+                res = max(res, i - j)
+            else:
+                d[cnt] = i
+        return res
