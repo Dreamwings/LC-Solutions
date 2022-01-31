ad349047dbcf82a81526a6c30514bfbe874ae9d8
Dreamwings <huyb2009@gmail.com>
2022-01-31 00:00:23 -0800

    Time: 1484 ms (11.50%), Space: 52.1 MB (59.56%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -6,6 +6,7 @@ class Solution(object):
         """
         
         ## S1:
+        ## Time: O(N)
         ## for any array whose length is l, the first missing positive must be in range [1,...,l+1]
         
         nums.append(0)
@@ -20,3 +21,31 @@ class Solution(object):
                 return i
         return n
 
+        ## S2: 
+        ## https://leetcode-cn.com/problems/first-missing-positive/solution/que-shi-de-di-yi-ge-zheng-shu-by-leetcode-solution/
+        ## Time: O(N)
+        
+        n = len(nums)
+        for i in range(n):
+            if nums[i] <= 0:
+                nums[i] = n + 1
+        
+        for i in range(n):
+            num = abs(nums[i])
+            if num <= n:
+                nums[num - 1] = -abs(nums[num - 1])
+        
+        for i in range(n):
+            if nums[i] > 0:
+                return i + 1
+        
+        return n + 1
+
+        ## S3:
+        
+        nums.sort()
+        res = 1
+        for num in nums:
+            if num == res:
+                res += 1
+        return res
