f54cc0b752049cb4b8cfcd69917ac872ef4965e4
Dreamwings <huyb2009@gmail.com>
2022-01-31 00:02:32 -0800

    Time: 1033 ms (41.32%), Space: 52.1 MB (77.64%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -6,25 +6,10 @@ class Solution(object):
         """
         
         ## S1:
-        ## Time: O(N)
-        ## for any array whose length is l, the first missing positive must be in range [1,...,l+1]
-        
-        nums.append(0)
-        n = len(nums)
-        for i in range(len(nums)): #delete those useless elements
-            if nums[i] < 0 or nums[i] >= n:
-                nums[i] = 0
-        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
-            nums[nums[i] % n] += n
-        for i in range(1,len(nums)):
-            if nums[i] / n==0:
-                return i
-        return n
-
-        ## S2: 
         ## https://leetcode-cn.com/problems/first-missing-positive/solution/que-shi-de-di-yi-ge-zheng-shu-by-leetcode-solution/
         ## Time: O(N)
         
+        # for any array whose length is N, the first missing positive must be in range [1,...,N+1]
         n = len(nums)
         for i in range(n):
             if nums[i] <= 0:
@@ -40,7 +25,25 @@ class Solution(object):
                 return i + 1
         
         return n + 1
-
+        
+        """
+        ## S2: 
+        ## Time: O(N)
+        ## for any array whose length is l, the first missing positive must be in range [1,...,l+1]
+        
+        nums.append(0)
+        n = len(nums)
+        for i in range(len(nums)): #delete those useless elements
+            if nums[i] < 0 or nums[i] >= n:
+                nums[i] = 0
+        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
+            nums[nums[i] % n] += n
+        for i in range(1,len(nums)):
+            if nums[i] / n==0:
+                return i
+        return n
+        
+        
         ## S3:
         
         nums.sort()
@@ -49,3 +52,5 @@ class Solution(object):
             if num == res:
                 res += 1
         return res
+        
+        """
