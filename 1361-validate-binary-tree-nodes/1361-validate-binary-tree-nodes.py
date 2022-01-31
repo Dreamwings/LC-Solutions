7712338cb927dffac52889b063024a20faf07e96
Dreamwings <huyb2009@gmail.com>
2022-01-31 11:27:26 -0800

    Time: 365 ms (28.57%), Space: 17.4 MB (22.45%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def validateBinaryTreeNodes(self, n, leftChild, rightChild):
+        """
+        :type n: int
+        :type leftChild: List[int]
+        :type rightChild: List[int]
+        :rtype: bool
+        """
+        ## S1: Topological Sort
+        ## Time: O(N)
+        ## Space: O(N)
+        
+        indeg = {x:0 for x in range(n)}
+        ch = set(leftChild + rightChild)
+        
+        for i in range(n):
+            if i in ch:
+                indeg[i] += 1
+                
+        q = collections.deque()
+        seen = set()
+        for k, v in indeg.items():
+            if v == 0:
+                q.append(k)
+                break
+        
+        while q:
+            x = q.popleft()
+            if x in seen:  # there must be a cycle
+                return False
+            seen.add(x)
+            
+            if leftChild[x] != -1:
+                q.append(leftChild[x])
+            if rightChild[x] != -1:
+                q.append(rightChild[x])
+                
+        return len(seen) == n
