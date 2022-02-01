47fa16fd1430a5d88abd4a84222ee6f1d5a18a2c
Dreamwings <huyb2009@gmail.com>
2022-01-31 18:18:59 -0800

    Time: 76 ms (62.23%), Space: 16.2 MB (9.44%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -6,6 +6,9 @@ class Solution(object):
         :rtype: int
         """
         
+        ## [1,2,4,4,3,3,0,9,2,3], k = 3
+        ## in the example, (3, 0) and (0, 3) are the same k-diff pairs
+        
         res = set()
         seen = set()
         
@@ -13,7 +16,7 @@ class Solution(object):
             if x - k in seen:
                 res.add((x - k, x))
             if x + k in seen:
-                res.add((x, x + k))
+                res.add((x, x + k))  # note here always keep smaller one in front
             seen.add(x)
         
         return len(res)
