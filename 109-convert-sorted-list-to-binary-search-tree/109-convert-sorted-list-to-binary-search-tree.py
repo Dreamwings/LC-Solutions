de71bce6becbbb0a4867941b4885180ba8d32491
Dreamwings <huyb2009@gmail.com>
2022-02-01 17:43:49 -0800

    Time: 128 ms (78.91%), Space: 25.6 MB (52.86%) - LeetHub

--- 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
+++ 109-convert-sorted-list-to-binary-search-tree/109-convert-sorted-list-to-binary-search-tree.py
@@ -1,16 +1,24 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        
+        ## Solution 1:
         
         if not head: return None
         
@@ -20,8 +28,8 @@ class Solution:
             n += 1
             p = p.next
         
+        # build the tree
         self.x = head
-        
         def dfs(l, r):
             if l > r: return
             
@@ -34,3 +42,45 @@ class Solution:
             return root
         
         return dfs(0, n-1)
+        
+        
+        """
+        ## Solution 2:
+        
+        if not head: return None
+        if not head.next: return TreeNode(head.val)
+        
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+        
+        def arrayToBST(arr):
+            n = len(arr)
+            if n == 0: return None
+            if n == 1: return TreeNode(arr[0])
+            m = n >> 1
+            root = TreeNode(arr[m])
+            root.left = arrayToBST(arr[:m])
+            root.right = arrayToBST(arr[m+1:])
+            return root
+        
+        return arrayToBST(arr)
+        
+        
+        ## Solution 3:
+        
+        if not head: return None
+        if not head.next: return TreeNode(head.val)
+        
+        prev, slow, fast = None, head, head
+        while fast and fast.next:
+            prev, slow, fast = slow, slow.next, fast.next.next
+        
+        root = TreeNode(slow.val)
+        prev.next = None
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(slow.next)
+        
+        return root
+        """
