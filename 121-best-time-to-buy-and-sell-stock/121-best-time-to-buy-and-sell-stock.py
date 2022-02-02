e03e2c2065220b28d2b3781b8b83a9eeddba5445
Dreamwings <huyb2009@gmail.com>
2022-02-01 17:48:02 -0800

    Time: 1201 ms (39.54%), Space: 22.4 MB (97.08%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        m, res = prices[0], 0
+        
+        for x in prices:
+            m = min(m, x)
+            res = max(res, x - m)
+        
+        return res
