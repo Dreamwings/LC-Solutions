9d7942d2983a5ec3e0195694c1a2759f11407ce4
Dreamwings <huyb2009@gmail.com>
2022-02-01 17:14:49 -0800

    Time: 144 ms (78.68%), Space: 15.6 MB (29.41%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.py
+++ 480-sliding-window-median/480-sliding-window-median.py
@@ -6,7 +6,8 @@ class Solution(object):
         :rtype: List[float]
         """
         from bisect import bisect, insort
-        
+        from heapq import heappush, heappop, heappushpop
+        """
         ## S1: insort
         ## Time: O(N logK) 
         ## https://leetcode.com/problems/sliding-window-median/discuss/96337/Python-SortedArray-(beats-100)-and-2-Heap(beats-90)-solution
@@ -20,6 +21,36 @@ class Solution(object):
             insort(win, nums[i])
         rv.append((win[k/2]+win[k/2-1])/2. if not odd else win[(k-1)/2]*1.)
         return rv
-    
-    
         
+        """
+        ## S2: Min Heap and Max Heap
+        ## Time: O(N logK)
+        
+        if not nums or not k:
+            return []
+        lo = [] # max heap
+        hi = [] # min heap
+        for i in range(k):
+            if len(lo) == len(hi):
+                heappush(hi, -heappushpop(lo, -nums[i]))
+            else:
+                heappush(lo, -heappushpop(hi, nums[i]))
+        ans = [float(hi[0])] if k & 1 else [(hi[0] - lo[0]) / 2.0]
+        to_remove = defaultdict(int)
+        for i in range(k, len(nums)): # right bound of window
+            heappush(lo, -heappushpop(hi, nums[i])) # always push to lo
+            out_num = nums[i-k]
+            if out_num > -lo[0]:
+                heappush(hi, -heappop(lo))
+            to_remove[out_num] += 1
+            while lo and to_remove[-lo[0]]:
+                to_remove[-lo[0]] -= 1
+                heappop(lo)
+            while to_remove[hi[0]]:
+                to_remove[hi[0]] -= 1
+                heappop(hi)
+            if k % 2:
+                ans.append(float(hi[0]))
+            else:
+                ans.append((hi[0] - lo[0]) / 2.0)
+        return ans
