595690d89513dff5bba958dba1e6b8dcc876fb75
Dreamwings <huyb2009@gmail.com>
2022-02-01 23:58:35 -0800

    Time: 40 ms (24.30%), Space: 13.5 MB (47.51%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -8,6 +8,19 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         
+        while m > 0 and n > 0:
+            # print(nums1)
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+            # print(nums1, m, n)
+            # note while condition must be m > 0 and n > 0 because we use index m - 1 and n - 1, so m - 1 >= 0, n - 1 >= 0 
+            
+        if n > 0: nums1[:n] = nums2[:n]
+            
         """
         if n == 0: return nums1
         if m == 0: nums1[:] = nums2[:]
@@ -26,15 +39,3 @@ class Solution(object):
         if j >= 0: nums1[:k+1] = nums2[:j+1]
         """
         
-        while m > 0 and n > 0:
-            # print(nums1)
-            if nums1[m-1] >= nums2[n-1]:
-                nums1[m+n-1] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[m+n-1] = nums2[n-1]
-                n -= 1
-            # print(nums1, m, n)
-            # note while condition must be m > 0 and n > 0 because we use index m - 1 and n - 1, so m - 1 >= 0, n - 1 >= 0 
-            
-        if n > 0: nums1[:n] = nums2[:n]
