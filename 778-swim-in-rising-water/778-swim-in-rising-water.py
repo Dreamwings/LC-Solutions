76b1233431542289ad637413a36a5a24a753b076
Dreamwings <huyb2009@gmail.com>
2022-02-06 12:21:40 -0800

    Time: 191 ms (39.13%), Space: 14.1 MB (99.85%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.py
@@ -0,0 +1,27 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        
+        from heapq import heappush, heappop
+        
+        n = len(grid)
+        if n == 1: return grid[0][0]
+        
+        hq = [(grid[0][0], 0, 0)]
+        grid[0][0] = -1
+        t = 0
+        dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        while hq:
+            v, x, y = heappop(hq)
+            t = max(t, v)
+            
+            if x == y == n - 1:
+                return t
+            
+            for dx, dy in dir:
+                i, j = x + dx, y + dy
+                if 0 <= i < n and 0 <= j < n and grid[i][j] >= 0:
+                    heappush(hq, (grid[i][j], i, j))
+                    grid[i][j] = -1
+                    
+        
