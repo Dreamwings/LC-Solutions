803d4df1ff760f3c4d2379868acb5886906f2a57
Dreamwings <huyb2009@gmail.com>
2022-02-06 16:39:28 -0800

    Time: 44 ms (56.86%), Space: 14.1 MB (94.86%) - LeetHub

--- 932-beautiful-array/932-beautiful-array.py
+++ 932-beautiful-array/932-beautiful-array.py
@@ -1,13 +1,12 @@
-class Solution(object):
-    def beautifulArray(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
+class Solution:
+    def beautifulArray(self, n: int) -> List[int]:
+        
         ## https://leetcode.com/problems/beautiful-array/discuss/1368125/Detailed-Explanation-with-Diagrams.-A-Collection-of-Ideas-from-Multiple-Posts.-Python3
         
-        """
-        ## S1: Iterative
+        
+        ## S1:
+        ## Time: O(NlogN)
+        ## Space: O(NlogN)
         
         res = [1]
         
@@ -15,15 +14,16 @@ class Solution(object):
             odd = [2 * x - 1 for x in res]
             even = [2 * x for x in res]
             res = odd + even
-            
-        # print(res)
+        
         return [x for x in res if x <= n]
+        
         """
         
-        ## S2: DFS
+        ## S2:
         ## https://leetcode-cn.com/problems/beautiful-array/solution/piao-liang-shu-zu-by-leetcode/
         ## Time: O(NlogN)
         ## Space: O(NlogN)
+        
         
         d = {1: [1]}
         
@@ -36,4 +36,4 @@ class Solution(object):
         
         return dfs(n)
         
-        
+        """
