167873eeb96a19c9ff6bd5868a3a65054bccad72
Dreamwings <huyb2009@gmail.com>
2022-02-07 18:06:42 -0800

    Time: 38 ms (10.36%), Space: 13.3 MB (99.14%) - LeetHub

---
+++ 68-text-justification/68-text-justification.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        
+        ## https://leetcode.com/problems/text-justification/discuss/24891/Concise-python-solution-10-lines.
+        
+        
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
