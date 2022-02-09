01922978dad8b5d74a9849785d19405336e37c26
Dreamwings <huyb2009@gmail.com>
2022-02-09 00:27:27 -0800

    Time: 84 ms (67.69%), Space: 13.8 MB (26.64%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -12,19 +12,42 @@ class Solution(object):
         ## Space: O(1)
         
         a, b = sorted((nums1, nums2), key=len)
-        m, n = len(a), len(b)
-        after = (m + n - 1) // 2
-        lo, hi = 0, m
-        while lo < hi:
-            i = (lo + hi) // 2
-            if after-i-1 < 0 or a[i] >= b[after-i-1]:
-                hi = i
+        m, n = len(a), len(b)  # m <= n
+        mid = (m + n - 1) >> 1
+        l, r = 0, m - 1
+        
+        while l <= r:
+            i = (l + r) >> 1
+            if mid - i - 1 < 0 or a[i] >= b[mid - i - 1]:
+                r = i - 1
             else:
-                lo = i + 1
-        i = lo
-        nextfew = sorted(a[i:i+2] + b[after-i:after-i+2])
-        # print(nextfew)
-        return (nextfew[0] + nextfew[1 - (m+n)%2]) / 2.0
+                l = i + 1
+                
+        mid_vals = sorted(a[l : l+2] + b[mid-l : mid - l + 2]) # four vals
+        # print(l, mid)
+        # print(mid_vals)
+        if (m + n) & 1:  # odd
+            return mid_vals[0]
+        else:
+            return (mid_vals[0] + mid_vals[1]) / 2.0
+        
+        
+#         a, b = sorted((nums1, nums2), key=len)
+#         m, n = len(a), len(b)
+#         after = (m + n - 1) // 2
+#         lo, hi = 0, m
+#         while lo < hi:
+#             i = (lo + hi) // 2
+#             if after-i-1 < 0 or a[i] >= b[after-i-1]:
+#                 hi = i
+#             else:
+#                 lo = i + 1
+#         i = lo
+        
+#         nextfew = sorted(a[i:i+2] + b[after-i:after-i+2])
+#         print(lo, after)
+#         print(nextfew)
+#         return (nextfew[0] + nextfew[1 - (m+n)%2]) / 2.0
     
     
         ## S2:
