c3e2c51619e2b610e2953fb7e24196f73654932e
Dreamwings <huyb2009@gmail.com>
2022-02-19 18:09:29 -0800

    Time: 326 ms (100.00%), Space: 17.4 MB (12.50%) - LeetHub

---
+++ 664-strange-printer/664-strange-printer.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def strangePrinter(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        ## S1: DP
+        ## https://leetcode.com/problems/strange-printer/discuss/106795/Python-Straightforward-DP-with-Explanation
+        
+        # shorten the original string, like reduce aaabbb to ab.
+        S = ''
+        for a, b in zip(s, '#' + s):
+            if a != b:
+                S += a
+        
+        mem = {}
+        
+        def dp(i, j):
+            # Let dp(i, j) be the number of turns needed to print S[i:j+1].
+            if i > j: return 0
+            if (i, j) in mem:
+                return mem[(i, j)]
+            cost = dp(i+1, j) + 1
+            for k in range(i+1, j+1):
+                if S[k] == S[i]:
+                    cost = min(cost, dp(i, k-1) + dp(k+1, j))
+            mem[(i, j)] = cost
+            return mem[(i, j)]
+        
+        return dp(0, len(S)-1)
