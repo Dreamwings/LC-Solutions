aa6ebc4fc4d771cd9eb3b482151291c4707ea563
Dreamwings <huyb2009@gmail.com>
2022-02-19 16:32:10 -0800

    Time: 1752 ms (100.00%), Space: 60.2 MB (80.00%) - LeetHub

--- 1776-car-fleet-ii/1776-car-fleet-ii.py
+++ 1776-car-fleet-ii/1776-car-fleet-ii.py
@@ -14,17 +14,24 @@ class Solution(object):
         res = [-1] * n
         
         for i in range(n-1, -1, -1):
+            # print(i, stack)
             p, s = cars[i]
             while stack:
                 j = stack[-1]
                 next_p, next_s = cars[j]
                 next_hit_t = res[j]
                 # two cases:
-                # 1. A catchs B and collides
-                # 2. A catchs B+C since B catches C and collides first
-                if s <= next_s or (next_hit_t > 0 and (next_p - p) * 1.0 / (s - next_s) >= next_hit_t):
+                # 1. A catchs B and collides, s > next_s
+                # 2. A catchs B+C since B catches C and collides first, next_hit_t is the time B catches C
+                #    tmp = (next_p - p) * 1.0 / (s - next_s)
+                #    tmp >= next_hit_t > 0, with this case, neet to pop last i from stack, but check the previous stack val
+                if s <= next_s:
+                    # Case 1: no collision
                     stack.pop()
-                else:
+                elif next_hit_t > 0 and (next_p - p) * 1.0 / (s - next_s) >= next_hit_t:
+                    # Case 2: no collision
+                    stack.pop()
+                else: # has collision
                     break
             
             if stack:
