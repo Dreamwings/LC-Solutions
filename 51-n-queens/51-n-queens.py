536770180edcbb039684dc3d61891d326669448f
Dreamwings <huyb2009@gmail.com>
2022-04-02 15:59:34 -0700

    Time: 64 ms (63.35%), Space: 13.6 MB (83.48%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,29 +1,36 @@
-class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        
+        ## S1: DFS Backtracking
         
         q, cols, xy_sum, xy_dif = [], [], [], []
         
         def dfs(q, cols, xy_sum, xy_dif):
-            x = len(cols)
+            x = len(cols)  # current len of cols actually is the row number to be visited next
             if x == n:
                 q.append(cols)
                 return
-        
-            for y in range(n):
+            
+            for y in range(n):  # y is the col number to be visited next
                 s, d = x + y, x - y
-                col_good = y not in cols
-                pos_good = s not in xy_sum
-                neg_good = d not in xy_dif
+                col_good = y not in cols  # this col is not occupied by previous Queens
+                pos_good = s not in xy_sum  # no Queens on positive 45 degree line
+                neg_good = d not in xy_dif  # no Queens on negative 45 degree line
                 if col_good and pos_good and neg_good:
                     dfs(q, cols + [y], xy_sum + [s], xy_dif + [d])
         
         dfs(q, cols, xy_sum, xy_dif)
-        res = []
         
+        res = []
         for col in q:
             row = []
             for y in col:
-                row.append('.' * y + 'Q' + '.' * (n-1-y))
+                row.append('.' * y + 'Q' + '.' * (n - 1 - y))
+            
             res.append(row)
             
         return res
