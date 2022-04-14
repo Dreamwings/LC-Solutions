c46897414a816bed29195f77b82d574c27241485
Dreamwings <huyb2009@gmail.com>
2022-04-13 23:57:47 -0700

    Time: 25 ms (99.91%), Space: 13.6 MB (73.38%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,10 +1,14 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        from collections import defaultdict
         
         if not s: return 0
-        seen = collections.defaultdict(int)  # to store the last index of char
-        i = 0
-        res = 0
+        seen = defaultdict(int)   # to store the last index of char
+        i, res = 0, 0
         
         for j, c in enumerate(s):
             if c in seen and i <= seen[c]:
@@ -13,5 +17,4 @@ class Solution:
                 res = max(res, j - i + 1)
             seen[c] = j
             
-        return res    
-        
+        return res
