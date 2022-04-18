de1a97d9bd591d31a6883decc93454c8ca8457f8
Dreamwings <huyb2009@gmail.com>
2022-04-17 17:26:14 -0700

    Time: 156 ms (69.04%), Space: 20.6 MB (21.87%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,5 +1,12 @@
-class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        ## S2:
+        ## Time: O(log(N/2))
         
         n = len(nums)
         l, r = 0, n - 1
@@ -19,5 +26,35 @@ class Solution:
             else:
                 r = m - 1
         
-        return nums[l]        
+        return nums[l]
+        
+        """
+        ## S1:
+        ## Time: O(log(N))
+         
+        n = len(nums)
+        l, r = 0, n - 1
+        
+        while l <= r:
+            m = (l + r) >> 1
+            m_odd = m & 1  # m odd, left side has even number of vals
+            # print(l, r, m)
+            if nums[m] == nums[m-1]:
+                # Case 1: [1, 1, 3, 3, 4, 5, 5]
+                if m_odd:  
+                    l = m + 1
+                # Case 2: [1, 1, 2, 3, 3, 5, 5, 7, 7]
+                else:
+                    r = m - 2
+            elif m + 1 < n and nums[m] == nums[m+1]:
+                # Case 3: [1, 1, 2, 3, 3, 5, 5]
+                if m_odd:
+                    r = m - 1
+                # Case 4: [1, 1, 3, 3, 5, 5, 6, 7, 7]
+                else:
+                    l = m + 2
+            else:
+                return nums[m]
             
+        return nums[m]
+        """
