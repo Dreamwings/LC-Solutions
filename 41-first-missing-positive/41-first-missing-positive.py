06ea646f1eeaf04e6737587f057932674311b9df
Dreamwings <huyb2009@gmail.com>
2022-05-04 00:26:07 -0700

    Time: 1625 ms (11.29%), Space: 59.6 MB (85.15%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,56 +1,21 @@
-class Solution(object):
-    def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
         
         ## S1:
-        ## https://leetcode-cn.com/problems/first-missing-positive/solution/que-shi-de-di-yi-ge-zheng-shu-by-leetcode-solution/
-        ## Time: O(N)
         
-        # for any array whose length is N, the first missing positive must be in range [1,...,N+1]
         n = len(nums)
+        
         for i in range(n):
             if nums[i] <= 0:
                 nums[i] = n + 1
         
         for i in range(n):
-            num = abs(nums[i])
-            if num <= n:
-                nums[num - 1] = -abs(nums[num - 1])
-        
+            x = abs(nums[i])
+            if x <= n:
+                nums[x - 1] = -abs(nums[x - 1])
+                
         for i in range(n):
             if nums[i] > 0:
                 return i + 1
         
         return n + 1
-        
-        """
-        ## S2: 
-        ## Time: O(N)
-        ## for any array whose length is l, the first missing positive must be in range [1,...,l+1]
-        
-        nums.append(0)
-        n = len(nums)
-        for i in range(len(nums)): #delete those useless elements
-            if nums[i] < 0 or nums[i] >= n:
-                nums[i] = 0
-        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
-            nums[nums[i] % n] += n
-        for i in range(1,len(nums)):
-            if nums[i] / n==0:
-                return i
-        return n
-        
-        
-        ## S3:
-        
-        nums.sort()
-        res = 1
-        for num in nums:
-            if num == res:
-                res += 1
-        return res
-        
-        """
