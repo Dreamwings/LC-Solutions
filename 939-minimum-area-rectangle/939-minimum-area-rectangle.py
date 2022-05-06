9b48f9b589e00e1db9ec828180e9e626ee555149
Dreamwings <huyb2009@gmail.com>
2022-05-06 00:22:34 -0700

    Time: 2627 ms (8.30%), Space: 14.2 MB (51.83%) - LeetHub

--- 939-minimum-area-rectangle/939-minimum-area-rectangle.py
+++ 939-minimum-area-rectangle/939-minimum-area-rectangle.py
@@ -1,26 +1,14 @@
-class Solution(object):
-    def minAreaRect(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
-        
-        ## S1:
-        ## https://leetcode-cn.com/problems/minimum-area-rectangle/solution/zui-xiao-mian-ji-ju-xing-by-leetcode/
-        ## Time: O(N^2)
-        
-        # find the two points of the diagonal 
+class Solution:
+    def minAreaRect(self, points: List[List[int]]) -> int:
         
         s = set([(x, y) for x, y in points])
         res = float('inf')
         
-        for i, (a, b) in enumerate(points):
-            for (x, y) in points[:i]:
+        for i, (x, y) in enumerate(points):
+            for (a, b) in points[:i]:
                 if a != x and b != y and (a, y) in s and (x, b) in s:
-                    # a rectangle exists
                     area = abs(a - x) * abs(b - y)
                     res = min(res, area)
-        
+                    
         if res == float('inf'): return 0
         return res
-        
