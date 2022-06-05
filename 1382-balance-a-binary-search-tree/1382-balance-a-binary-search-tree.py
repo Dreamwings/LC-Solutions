468d790b58c770502e57ef6bbf18062af572b4c1
Dreamwings <huyb2009@gmail.com>
2022-06-04 17:26:17 -0700

    Time: 319 ms (95.83%), Space: 19.4 MB (90.65%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -1,57 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def balanceBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def balanceBST(self, root: TreeNode) -> TreeNode:
         
-        ## S2: Store nodes instead of node.val
-        ## Time: O(N)
-        ## Space: O(N)
-        
-        def in_order(root):
-            if not root: return []
-            return in_order(root.left) + [root] + in_order(root.right)
+        def in_order(node):
+            if not node: return []
+            return in_order(node.left) + [node] + in_order(node.right)
         
         nodes = in_order(root)
         
-        def build_bst(l, r):  # use l, r pointers instead of array slicing can save memory
-            if l > r: return None
-            m = (l + r) // 2
-            root = nodes[m]
-            root.left = build_bst(l, m-1)
-            root.right = build_bst(m+1, r)
-            return root
-        
-        return build_bst(0, len(nodes)-1)
-        
-        """
-        ## S1: 
-        ## Time: O(N)
-        ## Space: O(N)
-        
-        def in_order(root):
-            if not root: return []
-            return in_order(root.left) + [root.val] + in_order(root.right)
-        
-        vals = in_order(root)
-        
-        def build_bst(l, r):  # use l, r pointers instead of array slicing can save memory
+        def build_bst(l, r):
             if l > r: return None
             
-            m = (l + r) // 2
-            root = TreeNode(vals[m])
-            root.left = build_bst(l, m-1)
-            root.right = build_bst(m+1, r)
+            m = (l + r) >> 1
+            root = nodes[m]
+            root.left = build_bst(l, m - 1)
+            root.right = build_bst(m + 1, r)
             return root
         
-        return build_bst(0, len(vals)-1)
-        
-        """
-        
+        return build_bst(0, len(nodes) - 1)
+            
