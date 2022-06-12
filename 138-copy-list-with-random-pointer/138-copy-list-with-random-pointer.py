63b1dcf70cfb64b574865e580670be7f18c46303
Dreamwings <huyb2009@gmail.com>
2022-06-12 01:05:49 -0700

    Time: 54 ms (41.54%), Space: 14.8 MB (81.42%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -1,25 +1,21 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, x, next=None, random=None):
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
 """
-class Solution(object):
-    def copyRandomList(self, head):
-        """
-        :type head: Node
-        :rtype: Node
-        """
-        
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
         
         from collections import defaultdict
         
         if not head: return None
-        # d = defaultdict(lambda: Node(0, next, random))
+        
         d = defaultdict(lambda: Node(0, None, None))
-        d[None] = None  # IMPORTANT, MUST HAVE!
+        d[None] = None
         x = head
         
         while x:
@@ -27,26 +23,5 @@ class Solution(object):
             d[x].next = d[x.next]
             d[x].random = d[x.random]
             x = x.next
-        
+            
         return d[head]
-        
-        """
-        
-        if not head: return None
-        d = {}
-        x = y = head
-        
-        while x:
-            d[x] = Node(x.val, None, None)
-            x = x.next
-        
-        while y:
-            if y.next:
-                d[y].next = d[y.next]
-            if y.random:
-                d[y].random = d[y.random]
-            y = y.next
-        
-        return d[head]
-        """
-        
