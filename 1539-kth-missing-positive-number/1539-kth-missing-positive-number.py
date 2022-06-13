127ca9aa6825876eddcba73c69261facebe33943
Dreamwings <huyb2009@gmail.com>
2022-06-13 00:12:05 -0700

    Time: 61 ms (69.40%), Space: 13.9 MB (83.76%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,18 +1,5 @@
-class Solution(object):
-    def findKthPositive(self, arr, k):
-        """
-        :type arr: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
-        ## S1: Binary Search
-        ## https://leetcode-cn.com/problems/kth-missing-positive-number/solution/di-k-ge-que-shi-de-zheng-zheng-shu-by-leetcode-sol/
-        
-        ## by arr[i], there are arr[i] - (i+1) missing numbers
-        ## let m = arr[i] - (i+1), there are m missing numbers before arr[i]
-        ## so we need to find the j-th index, so that before arr[j] it missing m1 numbers
-        ## at arr[j+1], it missing m2 numbers, k meet m1 <= k < m2
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
         
         lo, hi = 0, len(arr) - 1
         
@@ -24,9 +11,5 @@ class Solution(object):
                 hi = m - 1
         
         return lo + k
-        # should return this:
-        # arr[lo-1] + (k - (arr[lo-1] - (lo -1 + 1)))
-        # = arr[lo-1] + k - arr[lo-1] + lo
-        # = lo + k
         
         
