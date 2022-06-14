9a3215bf5f446d424b67678b4ff3da72aaf90623
Dreamwings <huyb2009@gmail.com>
2022-06-14 00:31:40 -0700

    Time: 36 ms (22.84%), Space: 13.3 MB (68.42%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -1,15 +1,19 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
         
-        ## S2:
-        
-        dummy = ListNode(-1)
+        dummy = ListNode(0)
         dummy.next = head
+        
         fast = slow = dummy
         
         for _ in range(n):
@@ -19,31 +23,7 @@ class Solution:
             fast = fast.next
             slow = slow.next
         
-        slow.next= slow.next.next
+        slow.next = slow.next.next
         return dummy.next
         
         
-        """
-        if not head: return None
-        
-        fast = slow = head
-        
-        # let fast pointer to move n step first
-        
-        for _ in range(n):
-            if fast:
-                fast = fast.next
-        
-        if not fast: return head.next
-        # this can handle the case: head = [1], n = 1, why?
-        
-        while fast.next:
-            # print(slow.val, fast.val)
-            fast = fast.next
-            slow = slow.next
-            
-        # print(slow.val)
-        slow.next = slow.next.next
-        
-        return head
-        """
