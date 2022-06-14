b591b9b70beaf3725d180fb66f4c0cb05f9a28e8
Dreamwings <huyb2009@gmail.com>
2022-06-14 01:24:56 -0700

    Time: 31 ms (58.32%), Space: 13.6 MB (50.23%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,8 +1,19 @@
-class Solution:
-    def myAtoi(self, s: str) -> int:
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        ## S1:
+        ## Need to consider special case:
+        ## 1. '+-12'
+        ## 2. '0000-42a123'
+        ## 3. '  '
+        ## 4. '   +0  123'
         
         neg = None
-        s = s.strip().lower()
+        s = s.strip()
         if not s: return 0
         
         if s[0] == '-':
@@ -19,10 +30,20 @@ class Solution:
             else:
                 v = 10 * v + int(c)
         
-        if neg:
-            v = -v
+        if neg: v = -v
         
         MAX, MIN = 2**31 - 1, -2**31
         if v > MAX: return MAX
         if v < MIN: return MIN
         return v
+        
+        
+        """
+        
+        ## S2:
+        
+        x = int(*re.findall('^[\+\-]?\d+', s.lstrip()))
+        return max(-2 ** 31, min(2 ** 31 - 1, x))
+    
+        """
+        
