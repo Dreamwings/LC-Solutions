b76b7acadb7ce7ad8d15e24a62f7026761933fff
Dreamwings <huyb2009@gmail.com>
2022-06-20 01:28:13 -0700

    Time: 159 ms (32.58%), Space: 14.4 MB (6.22%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,61 +1,70 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
         """
         ## S1:
-        ## https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2755/9-lines-O(log(min(mn)))-Python
-        ## Time: O(logmin(m,n)))
-        ## Space: O(1)
         
-        a, b = sorted((nums1, nums2), key=len)
-        m, n = len(a), len(b)  # m <= n
-        mid = (m + n - 1) >> 1
-        l, r = 0, m - 1
-        
-        while l <= r:
-            x = (l + r) >> 1
-            y = mid - x - 1
-            if y < 0 or a[x] >= b[y]:
-                r = x - 1
-            else:
-                l = x + 1
-                
-        mid_vals = sorted(a[l : l+2] + b[mid-l : mid - l + 2]) # four vals
-        # print(l, mid)
-        # print(mid_vals)
-        if (m + n) & 1:  # odd
-            return mid_vals[0]
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            a, b = nums2, nums1
+            m, n = n, m
         else:
-            return (mid_vals[0] + mid_vals[1]) / 2.0
+            a, b = nums1, nums2
         
+        mid = (m + n - 1) // 2
+        lo, hi = 0, m
+        # print('a:', a)
+        # print('b:', b)
+        # print('m, n, mid, lo, hi:', m, n, mid, lo, hi)
+        
+        while lo < hi:
+            i = (lo + hi) >> 1
+            j = mid - i - 1
+            # print('lo, hi, i, j:', lo, hi, i, j)
+            if j < 0 or a[i] >= b[j]:
+                hi = i
+            else:
+                lo = i + 1
+        
+        arr = sorted(a[lo : lo+2] + b[mid-lo : mid-lo+2])
+        # print('lo, mid-lo:', lo, mid-lo)
+        # print(arr)
+        if (m + n) & 1:
+            return arr[0]
+        else:
+            return (arr[0] + arr[1]) / 2.0
         """
-        ## S2: Find Kth Element of Two Sorted Arrays
-        ## https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2511/Intuitive-Python-O(log-(m%2Bn))-solution-by-kth-smallest-in-the-two-sorted-arrays-252ms
         
-        def kth(a, b, k):
-            # find the k-th element (0 based index)
-            # note that array slicing is O(N), so this is not real O(logN)
-            # but can change the array slicing with indices as also shown in the above link
-            
+        ## S2:
+        
+        def find_kth(a, b, k):
             if not a: return b[k]
             if not b: return a[k]
+            
             i, j = len(a) // 2, len(b) // 2
             ma, mb = a[i], b[j]
             
-            if i + j < k: 
-                if ma > mb: # a's median larger than b's, b's first half doesn't include k
-                    return kth(a, b[j+1:], k - j - 1)
+            if i + j < k:
+                if ma > mb:
+                    return find_kth(a, b[j+1:], k - (j + 1))
                 else:
-                    return kth(a[i+1:], b, k - i - 1)
+                    return find_kth(a[i+1:], b, k - (i + 1))
             else:
-                if ma > mb: # a's second half doesn't include k
-                    return kth(a[:i], b, k)
+                if ma > mb:
+                    return find_kth(a[:i], b, k)
                 else:
-                    return kth(a, b[:j], k)
-                    
+                    return find_kth(a, b[:j], k)
+            
         n = len(nums1) + len(nums2)
         if n & 1:
-            return kth(nums1, nums2, n//2)
+            return find_kth(nums1, nums2, n // 2)
         else:
-            return 0.5 * (kth(nums1, nums2, n//2) + kth(nums1, nums2, n//2 - 1))
-        
+            return (find_kth(nums1, nums2, n // 2 - 1) + find_kth(nums1, nums2, n // 2)) * 0.5
+            
+            
+            
+            
+            
+            
+            
         
