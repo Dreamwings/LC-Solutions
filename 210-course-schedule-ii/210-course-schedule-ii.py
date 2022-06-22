12c859574ed9b9826e99f27dba073a6e88123d52
Dreamwings <huyb2009@gmail.com>
2022-06-22 00:05:00 -0700

    Time: 103 ms (93.20%), Space: 15.5 MB (69.25%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,68 +1,35 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
         from collections import defaultdict, deque
         
         ## Solution 1: BFS/Topological Sort
         
-        graph = defaultdict(list)
-        indeg = {x: 0 for x in range(numCourses)}
+        g = defaultdict(list)
+        indeg = {i: 0 for i in range(numCourses)}
         
         for x, y in prerequisites:
-            graph[y].append(x)
+            g[y].append(x)
             indeg[x] += 1
         
         q = deque()
+        
         for k, v in indeg.items():
             if v == 0:
                 q.append(k)
         
-        if not q: return []
         res = []
+        if not q: return res
         
         while q:
             x = q.popleft()
             res.append(x)
-            for y in graph[x]:
+            for y in g[x]:
                 indeg[y] -= 1
                 if indeg[y] == 0:
                     q.append(y)
-                    
+       
         if len(res) == numCourses:
             return res
-        
         return []
-        
-        """
-        
-        ## Solution 2: DFS
-        res = []
-        graph = defaultdict(set)
-        out_deg = [0] * numCourses
-        q = []
-        
-        for u, v in prerequisites:
-            graph[v].add(u)
-            out_deg[u] += 1
-        
-        for i in range(numCourses):
-            if out_deg[i] == 0:
-                q.append(i)
-        
-        if len(q) == 0: return res
-        
-        while q:
-            x = q.pop()
-            res.append(x)
-            for y in graph[x]:
-                out_deg[y] -= 1
-                if out_deg[y] == 0:
-                    q.append(y)
-        
-        if len(res) == numCourses: return res
-        return []
-        """
+            
