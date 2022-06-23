3cf97fae4a5956ee04ea8e6c09808163d4fcefe3
Dreamwings <huyb2009@gmail.com>
2022-06-22 17:08:45 -0700

    Time: 334 ms (74.99%), Space: 14.1 MB (80.15%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,49 +1,17 @@
-class Solution(object):
-    def findTargetSumWays(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
         from collections import defaultdict
         
-        
-        ## S 2: BFS
-        ## Special level traversal BFS
-        
-        cnt = defaultdict(int)
+        cnt = defaultdict(int)  # target value: number of expressions
         cnt[0] = 1
         
         for x in nums:
-            cur_cnt = defaultdict(int)
-            for y, c in cnt.items():
-                cur_cnt[y + x] += c
-                cur_cnt[y - x] += c
-            cnt = cur_cnt
-            
+            new = defaultdict(int)
+            for y, v in cnt.items():
+                new[y + x] += v
+                new[y - x] += v
+            cnt = new
+        
         return cnt[target]
         
-        """
-        
-        ## S 1: DFS + Memorization (TLE)
-        ## https://leetcode.com/problems/target-sum/discuss/455024/DP-IS-EASY!-5-Steps-to-Think-Through-DP-Questions.
-        
-        
-        def dfs(i, target, path_sum):
-            if i == n and path_sum == target:
-                return 1
-            elif i == n:
-                return 0
-            
-            pos = dfs(i+1, target, path_sum + nums[i])
-            neg = dfs(i+1, target, path_sum - nums[i])
-            
-            return pos + neg
-        
-        n = len(nums)
-        res = dfs(0, target, 0)
-        
-        return res
-        
-        
-        """
