a0cdf0d3e94b2265149a67d8c895d8bdac9150db
Dreamwings <huyb2009@gmail.com>
2022-06-23 19:44:51 -0700

    Time: 263 ms (85.00%), Space: 30.1 MB (54.71%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def removeSubfolders(self, folder):
-        """
-        :type folder: List[str]
-        :rtype: List[str]
-        """
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
         
         folder.sort()
         res = [folder[0]]
-        i = 1
         
-        while i < len(folder):
-            if res[-1] not in folder[i]:
-                res.append(folder[i])
-            elif folder[i].index(res[-1]) != 0:
-                res.append(folder[i])
+        for s in folder[1:]:
+            x = res[-1]
+            if x not in s:
+                res.append(s)
+            elif s.index(x) != 0:
+                res.append(s)
             else:
-                x = len(res[-1])
-                if folder[i][x] != '/':
-                    res.append(folder[i])
-            i += 1
+                n = len(x)
+                if s[n] != '/':
+                    res.append(s)
         
         return res
         
