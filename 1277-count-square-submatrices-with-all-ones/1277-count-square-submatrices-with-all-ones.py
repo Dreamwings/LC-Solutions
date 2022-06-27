1a38edc82106d098ba5be399d53388f327ea69ad
Dreamwings <huyb2009@gmail.com>
2022-06-26 21:43:02 -0700

    Time: 612 ms (96.80%), Space: 16.2 MB (73.85%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,28 +1,15 @@
-class Solution(object):
-    def countSquares(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
         
         ## S1: DP
-        ## https://leetcode.com/problems/count-square-submatrices-with-all-ones/discuss/643429/Python-DP-Solution-%2B-Thinking-Process-Diagrams-(O(mn)-runtime-O(1)-space)
-        ## Time: O(MN)
-        ## Space: O(1)
         
         m, n = len(matrix), len(matrix[0])
         res = 0
-        # use matrix itself as the DP matrix to store DP values
-        # dp[i][j]: 1. the largest size of squares with (i, j) cell as the right bottom cell.
-        #           2. the number of squares with (i, j) cell as the right bottom cell.
         
         for i in range(m):
             for j in range(n):
-                # for the first row (i==0) or the first col (j==0), just keep the value
-                # for other cells, update the value with the DP equation
                 if matrix[i][j] and i > 0 and j > 0:
                     matrix[i][j] = 1 + min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1])
                 
                 res += matrix[i][j]
-                    
         return res
