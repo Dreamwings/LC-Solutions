c71b0c41a5691eb5a0a2e752cb2288cc1f1b1b81
Dreamwings <huyb2009@gmail.com>
2022-06-26 17:14:43 -0700

    Time: 382 ms (84.05%), Space: 24.9 MB (64.11%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,13 +1,7 @@
-class Solution(object):
-    def minDifference(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
         
         ## S2: Heapq
-        ## Time: O(NlogK)
-        ## Space: O(1)
         
         from heapq import nlargest, nsmallest
         
@@ -15,32 +9,12 @@ class Solution(object):
         if n <= 4: return 0
         
         a = nlargest(4, nums)
+        a = a[::-1]  # now a is sorted from small to large
         b = nsmallest(4, nums)
+        # print(a, b)
         res = float('inf')
         
-        for x, y in zip(a[::-1], b):
+        for x, y in zip(a, b):
             res = min(res, x - y)
         
         return res
-        
-        """
-        
-        ## S1: Sorting
-        ## Time: O(NlogN)
-        ## Space: O(1)
-        
-        # remove 3 vals from left/right with sorted arr
-        # consider different cases and get their own min diff
-        n = len(nums)
-        if n <= 4: return 0 # can always remove 3 largest/smallest vals
-        
-        nums.sort()
-        d1 = nums[-4] - nums[0]  # remove 3 largests
-        d2 = nums[-3] - nums[1]  # remove 2 largests and 1 smallest
-        d3 = nums[-2] - nums[2]  # remove 1 largests and 2 smallest
-        d4 = nums[-1] - nums[3]  # remove 3 smallests
-        
-        return min(d1, d2, d3, d4)
-        
-        """
-        
