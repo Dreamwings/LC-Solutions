4ae66a505f609d841473648e583e4e0be6b12de9
Dreamwings <huyb2009@gmail.com>
2022-06-26 17:48:42 -0700

    Time: 1286 ms (93.33%), Space: 64.9 MB (93.33%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -1,9 +1,16 @@
-class Solution:
-    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
-        
-        
+class Solution(object):
+    def findMaxValueOfEquation(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
         # yi + yj + |xi - xj| = yi + yj - xi + xj because points is sorted by x and i < j
         # = (yi - xi) + yj + xj
+        
+        ## S2: Mono Queue
+        ## Time: O(N)
+        ## Space: O(N)
         
         q = collections.deque()
         res = float('-inf')
@@ -21,3 +28,26 @@ class Solution:
             q.append((x, y))
             
         return res
+    
+        """
+        
+        ## S1: Priority Queue
+        ## Time: O(NlogN)
+        ## Space: O(N)
+        ## yi + yj + |xi - xj| = yi - xi + yj + xj because points is sorted
+        ## to get max (yi - xi), use a max heap with -(yi - xi) = (xi - yi)
+        
+        from heapq import heappush, heappop
+        
+        hq = []  # store (xi - yi, xi)
+        res = float('-inf')
+        
+        for x, y in points:
+            while hq and x - hq[0][1] > k:
+                heappop(hq)
+            if hq:
+                res = max(res, -hq[0][0] + x + y)
+            heappush(hq, (x - y, x))
+        
+        return res
+        """
