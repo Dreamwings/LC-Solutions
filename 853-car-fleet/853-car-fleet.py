ea555423685eedd01a2466527f36adb0a30edabf
Dreamwings <huyb2009@gmail.com>
2022-06-26 19:03:57 -0700

    Time: 1699 ms (22.96%), Space: 36.7 MB (29.69%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -1,31 +1,16 @@
-class Solution(object):
-    def carFleet(self, target, position, speed):
-        """
-        :type target: int
-        :type position: List[int]
-        :type speed: List[int]
-        :rtype: int
-        """
-        
-        ## S1: Sort
-        ## https://leetcode.com/problems/car-fleet/discuss/139850/C%2B%2BJavaPython-Straight-Forward
-        ## https://leetcode-cn.com/problems/car-fleet/solution/che-dui-by-leetcode/
-        ## Time: O(NlogN)
-        ## Space: O(N)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         
         cars = sorted(zip(position, speed))
-        times = [float(target - p) / s for p, s in cars]
-        res = k = 0
-        # k is the needed time for the closest fleet in front of the current car
+        times = [1.0 * (target - p)/s for p, s in cars]
+        # print(cars, times)
+        prev = 0
+        res = 0
         
-        for t in times[::-1]:
-            if t > k: 
-                # means the current car can NOT catch up the fleet in front of it
-                # it must be a new fleet
+        for t in times[::-1]: # consider the one closest to the target first
+            if t > prev: # current car need more time than the prev one, not catch up
                 res += 1
-                k = t
-            
+                prev = t
+        
         return res
-        
-        
         
