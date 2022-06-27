5b7e8b82d6f926a487fb2605361840006869ff40
Dreamwings <huyb2009@gmail.com>
2022-06-26 17:47:24 -0700

    Time: 2173 ms (24.45%), Space: 60.7 MB (16.06%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -1,50 +1,23 @@
-class Solution(object):
-    def findMaxValueOfEquation(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
+        
+        
         # yi + yj + |xi - xj| = yi + yj - xi + xj because points is sorted by x and i < j
         # = (yi - xi) + yj + xj
         
-        ## S2: Mono Queue
-        ## Time: O(N)
-        ## Space: O(N)
-        
-        q = collections.deque()  # store (yi - xi, xi)
+        q = collections.deque()
         res = float('-inf')
         
         for x, y in points:
-            while q and x - q[0][1] > k:
+            while q and x - q[0][0] > k:
                 q.popleft()
+            
             if q:
-                res = max(res, q[0][0] + x + y)
-            while q and q[-1][0] < y - x:
+                res = max(res, q[0][1] - q[0][0] + x + y)
+            
+            while q and q[-1][1] - q[-1][0] < y - x:
                 q.pop()
-            q.append((y - x, x))
+            
+            q.append((x, y))
             
         return res
-    
-        """
-        
-        ## S1: Priority Queue
-        ## Time: O(NlogN)
-        ## Space: O(N)
-        ## yi + yj + |xi - xj| = yi - xi + yj + xj because points is sorted
-        ## to get max (yi - xi), use a max heap with -(yi - xi) = (xi - yi)
-        
-        from heapq import heappush, heappop
-        
-        hq = []  # store (xi - yi, xi)
-        res = float('-inf')
-        
-        for x, y in points:
-            while hq and x - hq[0][1] > k:
-                heappop(hq)
-            if hq:
-                res = max(res, -hq[0][0] + x + y)
-            heappush(hq, (x - y, x))
-        
-        return res
-        """
