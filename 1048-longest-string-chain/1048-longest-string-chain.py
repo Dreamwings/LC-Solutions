fc1512ba7599b5bd5661a79960fe7f2c21c64aaf
Dreamwings <huyb2009@gmail.com>
2022-06-26 23:39:42 -0700

    Time: 257 ms (47.28%), Space: 14.2 MB (98.85%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,26 +1,18 @@
-class Solution(object):
-    def longestStrChain(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
         
         ## S1: DP
-        ## https://leetcode.com/problems/longest-string-chain/discuss/1213876/Python-3-solutions-LIS-DP-Top-down-DP-Bottom-up-DP-Clean-and-Concise
-        ## Time: O(NlogN + N*L*L)
-        ## Space: O(N)
         
-        dp = collections.defaultdict(int)
         words.sort(key=len)
+        dp = collections.defaultdict(int)
         res = 0
         
         for w in words:
             dp[w] = 1
             for i in range(len(w)):
                 pre = w[:i] + w[i+1:]
-                if pre in dp and dp[w] < dp[pre] + 1:
-                    dp[w] = dp[pre] + 1
+                if pre in dp:
+                    dp[w] = max(dp[w], dp[pre] + 1)
+                    
             res = max(res, dp[w])
-        
         return res
-        
