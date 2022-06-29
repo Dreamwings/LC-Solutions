4d017a0e5195b2d71422b68b3ef690f937771507
Dreamwings <huyb2009@gmail.com>
2022-06-28 17:48:34 -0700

    Time: 805 ms (46.82%), Space: 15.9 MB (31.32%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,6 +1,11 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         
+        ##
+        ## e.g.
+        ## s = "abcde"
+        ## words = ["a","bb","acd","ace", "bde", "ce"]
+        
         from collections import defaultdict
         
         d = defaultdict(list)
@@ -8,7 +13,7 @@ class Solution:
         
         for w in words:
             d[w[0]].append(w)
-        
+        # print(d)
         for c in s:
             p = d[c]
             del d[c]
@@ -17,5 +22,5 @@ class Solution:
                     res += 1
                 else:
                     d[w[1]].append(w[1:])
-                
+            # print(d)
         return res
