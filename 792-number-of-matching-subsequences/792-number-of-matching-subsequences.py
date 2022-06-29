b73c7c0205ad567cf642fd520aba8fd5f407f3d1
Dreamwings <huyb2009@gmail.com>
2022-06-28 17:41:50 -0700

    Time: 884 ms (41.24%), Space: 15.4 MB (94.43%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,29 +1,21 @@
-class Solution(object):
-    def numMatchingSubseq(self, s, words):
-        """
-        :type s: str
-        :type words: List[str]
-        :rtype: int
-        """
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         
-        ## S1:
-        ## to understand: https://leetcode-cn.com/problems/number-of-matching-subsequences/solution/pi-pei-zi-xu-lie-de-dan-ci-shu-by-leetcode/
-        ## https://leetcode.com/problems/number-of-matching-subsequences/discuss/329381/Python-Solution-With-Detailed-Explanation
+        from collections import defaultdict
         
-        d = collections.defaultdict(list)
+        d = defaultdict(list)
         res = 0
         
         for w in words:
             d[w[0]].append(w)
         
-        for x in s:
-            possible_words = d[x]
-            d[x] = []  # remove this item, but will add new ones
-            
-            for w in possible_words:
-                if len(w) == 1:  # there is a match for single letter string
+        for c in s:
+            p = d[c]
+            del d[c]
+            for w in p:
+                if len(w) == 1:
                     res += 1
                 else:
                     d[w[1]].append(w[1:])
-        
+                
         return res
