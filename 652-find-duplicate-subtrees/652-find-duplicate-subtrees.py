8f8402893f7f610b892e276cff7f0278f696a339
Dreamwings <huyb2009@gmail.com>
2022-06-29 23:25:53 -0700

    Time: 96 ms (58.51%), Space: 23.6 MB (42.03%) - LeetHub

--- 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.py
@@ -1,22 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def findDuplicateSubtrees(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[TreeNode]
-        """
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
         
         d = collections.Counter()
         res = []
         
         def dfs(node):
             if not node: return '#'
-            key = '{}, {}, {}'.format(node.val, dfs(node.left), dfs(node.right))
+            a = [str(node.val), dfs(node.left), dfs(node.right)]
+            key = ','.join(a)
             d[key] += 1
             if d[key] == 2:
                 res.append(node)
@@ -24,4 +21,5 @@ class Solution(object):
         
         dfs(root)
         return res
+        
         
