757f00119d66a28e3d309c776b422536ff5a04c0
Dreamwings <huyb2009@gmail.com>
2022-06-30 00:58:04 -0700

    Time: 2150 ms (83.33%), Space: 20.6 MB (55.56%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -4,26 +4,6 @@ class Solution(object):
         :type stoneValue: List[int]
         :rtype: str
         """
-        """
-        ## S1: DP
-        ## https://leetcode.com/problems/stone-game-iii/discuss/564260/JavaC%2B%2BPython-DP-O(1)-Space
-        ## Time: O(N)
-        ## Space: O(1)
-        
-        dp = [0] * 3
-        n = len(stoneValue)
-        
-        for i in range(n-1, -1, -1):
-            dp[i % 3] = max(sum(stoneValue[i : i+k]) - dp[(i + k) % 3] for k in [1, 2, 3])
-        
-        if dp[0] == 0:
-            return 'Tie'
-        elif dp[0] > 0:
-            return 'Alice'
-        else:
-            return 'Bob'
-        
-        """
         
         ## S2: DP
         ## https://leetcode-cn.com/problems/stone-game-iii/solution/shi-zi-you-xi-iii-by-leetcode-solution/
@@ -47,4 +27,24 @@ class Solution(object):
         else:
             return "Bob"
         
+        """
+        ## S1: DP
+        ## https://leetcode.com/problems/stone-game-iii/discuss/564260/JavaC%2B%2BPython-DP-O(1)-Space
+        ## Time: O(N)
+        ## Space: O(1)
+        
+        dp = [0] * 3
+        n = len(stoneValue)
+        
+        for i in range(n-1, -1, -1):
+            dp[i % 3] = max(sum(stoneValue[i : i+k]) - dp[(i + k) % 3] for k in [1, 2, 3])
+        
+        if dp[0] == 0:
+            return 'Tie'
+        elif dp[0] > 0:
+            return 'Alice'
+        else:
+            return 'Bob'
+        
+        """
         
