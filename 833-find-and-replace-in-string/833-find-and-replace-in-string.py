286cc54f3e3457f1fbbb04411a0fb76a4c2b995a
Dreamwings <huyb2009@gmail.com>
2022-06-30 01:19:46 -0700

    Time: 51 ms (62.91%), Space: 13.9 MB (57.18%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def findReplaceString(self, s, indices, sources, targets):
-        """
-        :type s: str
-        :type indices: List[int]
-        :type sources: List[str]
-        :type targets: List[str]
-        :rtype: str
-        """
+class Solution:
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
         
         ## S1: Scan from Right to Left
-        ## Time: O(N)
-        ## Space: O(1)
         
         for i, x, y in sorted(zip(indices, sources, targets), reverse=True):
-            if s[i : i+len(x)] == x:
-                s = s[:i] + y + s[i+len(x):]
-                
+            j = i + len(x)
+            if s[i:j] == x:
+                s = s[:i] + y + s[j:]
+        
         return s
