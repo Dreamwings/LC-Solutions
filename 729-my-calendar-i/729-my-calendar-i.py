2ebad18f18afbc54f1b409262649c15a90b7579a
Dreamwings <huyb2009@gmail.com>
2022-06-30 12:54:47 -0700

    Time: 438 ms (68.99%), Space: 14.4 MB (41.14%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,31 @@
+from sortedcontainers import SortedList
+    
+class MyCalendar(object):
+    
+    def __init__(self):
+        self.a = SortedList()
+        
+
+    def book(self, start, end):
+        
+        i = SortedList.bisect_right(self.a, start)
+        j = SortedList.bisect_left(self.a, end)
+        
+        if i == j and i % 2 == 0: # i is even
+            self.a.add(start)
+            self.a.add(end)
+            return True
+        
+        return False
+        
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
