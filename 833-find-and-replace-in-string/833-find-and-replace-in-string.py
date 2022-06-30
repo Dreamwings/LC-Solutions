c33f3e032928b240aaad1f6409155e75106455db
Dreamwings <huyb2009@gmail.com>
2022-06-30 01:20:01 -0700

    Time: 36 ms (51.40%), Space: 13.5 MB (82.12%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,7 +1,16 @@
-class Solution:
-    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+class Solution(object):
+    def findReplaceString(self, s, indices, sources, targets):
+        """
+        :type s: str
+        :type indices: List[int]
+        :type sources: List[str]
+        :type targets: List[str]
+        :rtype: str
+        """
         
         ## S1: Scan from Right to Left
+        ## Time: O(N)
+        ## Space: O(1)
         
         for i, x, y in sorted(zip(indices, sources, targets), reverse=True):
             j = i + len(x)
