21a6b0c200b0520f06ba89d8ac405614d66b0fc4
Dreamwings <huyb2009@gmail.com>
2022-06-30 00:57:36 -0700

    Time: 2612 ms (72.22%), Space: 20.6 MB (55.56%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type stoneValue: List[int]
         :rtype: str
         """
-        
+        """
         ## S1: DP
         ## https://leetcode.com/problems/stone-game-iii/discuss/564260/JavaC%2B%2BPython-DP-O(1)-Space
         ## Time: O(N)
@@ -22,4 +22,29 @@ class Solution(object):
             return 'Alice'
         else:
             return 'Bob'
+        
+        """
+        
+        ## S2: DP
+        ## https://leetcode-cn.com/problems/stone-game-iii/solution/shi-zi-you-xi-iii-by-leetcode-solution/
+        
+        # first calculate suffix sum of stoneValue
+        n = len(stoneValue)
+        suf_sum = [0] * (n-1) + [stoneValue[-1]]
+        
+        for i in range(n-2, -1, -1):
+            suf_sum[i] = suf_sum[i+1] + stoneValue[i]
+        
+        dp = [0] * (n+1)
+        for i in range(n-1, -1, -1):
+            dp[i] = suf_sum[i] - min(dp[i+1 : i+4])
+            # print(dp)
+            
+        if dp[0] * 2 == suf_sum[0]:
+            return "Tie"
+        elif dp[0] * 2 > suf_sum[0]:
+            return "Alice"
+        else:
+            return "Bob"
+        
         
