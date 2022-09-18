eb723d3298bc537fc57dbd715783b51c11183fe5
Dreamwings <huyb2009@gmail.com>
2022-09-18 14:17:13 -0700

    Time: 294 ms (18.16%), Space: 16 MB (81.26%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -6,20 +6,19 @@ class Solution:
         ## every time move the pointer with a lower bound
         
         n = len(height)
-        if n <= 2: return 0
         i, j = 0, n - 1
         l, r = height[0], height[n-1]
-        rain = 0
+        res = 0
         
         while i < j:
             h = min(l, r)
-            if l == h:  # left side lower
-                rain += max(0, h - height[i])
+            if l == h:
+                res += max(0, h - height[i])
                 i += 1
-                l = max(l, height[i])
-            else:  # right side lower
-                rain += max(0, h - height[j])
+                l = max(height[i], h)
+            else:
+                res += max(0, h - height[j])
                 j -= 1
-                r = max(r, height[j])
-                
-        return rain
+                r = max(height[j], h)
+        
+        return res
