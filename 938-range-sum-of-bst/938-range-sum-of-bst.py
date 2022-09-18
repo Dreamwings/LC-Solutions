a9fcc9416e1ddd881d884e400a3bf95b0865f34e
Dreamwings <huyb2009@gmail.com>
2022-09-18 00:58:51 -0700

    Time: 629 ms (6.46%), Space: 23.1 MB (50.89%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -1,58 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def rangeSumBST(self, root, low, high):
-        """
-        :type root: TreeNode
-        :type low: int
-        :type high: int
-        :rtype: int
-        """
-        ## S2:  Iterative BFS
-        ## Time: O(N)
-        ## Space: O(N)
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         
         from collections import deque
         
-        if not root: return 0
-        
+        q = deque()
+        q.append(root)
         res = 0
-        q = deque([root])
         
         while q:
             x = q.popleft()
             if low <= x.val <= high:
                 res += x.val
-            if x.left:
+            if x.val > low and x.left:
                 q.append(x.left)
-            if x.right:
+            if x.val < high and x.right:
                 q.append(x.right)
         
         return res
-        
-        """
-        ## S1: Recursive DFS
-        ## Time: O(N)
-        ## Space: O(N)
-        
-        res = 0
-        if not root: return res
-        
-        if low <= root.val <= high:
-            res += root.val
-        
-        l = self.rangeSumBST(root.left, low, high)
-        r = self.rangeSumBST(root.right, low, high)
-        
-        return res + l + r
-        
-        """
-        
         
         
         
