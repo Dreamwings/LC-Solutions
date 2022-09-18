cd300af2563e84fb58dfe3b953f22fab6f76ae65
Dreamwings <huyb2009@gmail.com>
2022-09-18 16:19:17 -0700

    Time: 1083 ms (44.99%), Space: 25.5 MB (28.08%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -8,13 +8,36 @@ class Solution(object):
         from heapq import nlargest, heappush, heappop
         
         
+        ## S3: QuickSelect:
+        # https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/1019513/Python-QuickSelect-average-O(n)-explained
+        # Time: O(N)
+        # Space: O(N)
+        
+        if not nums: return
+        pivot = random.choice(nums)
+        gt_vals = [x for x in nums if x > pivot]
+        eq_vals = [x for x in nums if x == pivot]
+        st_vals = [x for x in nums if x < pivot]
+        
+        GL, EL = len(gt_vals), len(eq_vals)
+        if k <= GL:
+            return self.findKthLargest(gt_vals, k)
+        elif k <= GL + EL:
+            return pivot
+        else:
+            return self.findKthLargest(st_vals, k - GL - EL)
+        
+        
+        
+        """
         ## Solution 1:
+        # O(NlogK)
         
         return nlargest(k, nums)[-1]
-        """
+        
         
         ## Solution 2:
-        # O(nlogk)
+        # O(NlogK)
         
         q = []
         
