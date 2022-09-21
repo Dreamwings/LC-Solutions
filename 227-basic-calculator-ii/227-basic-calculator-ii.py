80325e55c1d56f0b8cafe18c145875d1d75ca7bc
Dreamwings <huyb2009@gmail.com>
2022-09-20 23:27:20 -0700

    Time: 106 ms (79.52%), Space: 15.9 MB (24.83%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,31 +1,25 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def calculate(self, s: str) -> int:
         
-        s += '+'
-        v, pre_op, stack = 0, '+', []
+        s += "+"
+        pre_op, v, stack = "+", 0, []
         
         for c in s:
             if c.isdigit():
                 v = 10 * v + int(c)
-            elif c in '+-*/':
-                if pre_op == '+':
+            elif c in "+-*/":
+                if pre_op == "+":
                     stack.append(v)
-                elif pre_op == '-':
+                elif pre_op == "-":
                     stack.append(-v)
-                elif pre_op == '*':
+                elif pre_op == "*":
                     x = stack.pop()
                     stack.append(x * v)
-                elif pre_op == '/':
+                elif pre_op == "/":
                     x = stack.pop()
-                    # The following is only for Python 2
-                    # Python 3 can handle it well with: int(x/y)
-                    stack.append(int(x * 1.0 / v))
+                    stack.append(int(x/v))
+                
                 v, pre_op = 0, c
-                    
-        return sum(stack)            
-            
-            
+        
+        return sum(stack)
+                
