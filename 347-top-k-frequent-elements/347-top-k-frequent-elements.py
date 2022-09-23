5f4224a4e46e475abc9dd9b36627e56db7c2e617
Dreamwings <huyb2009@gmail.com>
2022-09-23 11:30:40 -0700

    Time: 276 ms (9.00%), Space: 19.7 MB (5.92%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,69 +1,30 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         from collections import Counter
-        from heapq import nlargest, heappush, heappop
+        """
+        ## S1: Heapq
+        ## S2: Heapq.nlargest
+        ## S3: dict.most_common(k)
         
-        ## S4: Bucket Sort
+        d = Counter(nums).most_common(k)
+        return [x for x, f in d]
+        
+        """
+        ## S4: Bucket sort
         
         d = Counter(nums)
         n = len(nums)
-        bucket = [[] for _ in range(n + 1)]
+        b = [[] for _ in range(n+1)]  # bucket
         
-        for x, freq in d.items():
-            bucket[freq].append(x)
+        for v, f in d.items():
+            b[f].append(v)
         
-        # flat_list = list(itertools.chain(*bucket))
-        # return flat_list[::-1][:k]
-        # print(bucket)
         res = []
         for i in range(n, -1, -1):
-            if bucket[i]:
-                res += bucket[i]
-            if len(res) >= k:
-                break
-                
-        return res[:k]
-        
-        """
-        ## Solution 1:
-        
-        d = Counter(nums)
-        
-        freq = []
-        
-        for key, v in d.items():
-            freq.append((v, key))
-        
-        freq = nlargest(k, freq)
-        
-        res = [y for x, y in freq]
+            if b[i]:
+                res += b[i]
+                if len(res) >= k:
+                    break
         
         return res[:k]
-        
-        
-        ## Solution 2:
-        
-        d = Counter(nums)
-        freq = [(v, key) for key, v in d.items()]
-        
-        res = []
-        for v, num in freq:
-            heappush(res, (v, num))
-            if len(res) > k:
-                heappop(res)
-        
-        return [y for x, y in res]
-        
-        
-        ## Solution 3:
-        
-        d = Counter(nums).most_common(k)
-        
-        return [x for x, v in d]
-        """
