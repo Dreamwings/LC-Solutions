6f28ab70e7bbed8de4773127dd1a615b92647a88
Dreamwings <huyb2009@gmail.com>
2022-09-23 17:47:49 -0700

    Time: 37 ms (90.83%), Space: 14.1 MB (97.59%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -1,45 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def verticalTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        
-        from collections import defaultdict
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         if not root: return []
         
-        d = defaultdict(list)
-        res = []
+        d = collections.defaultdict(list)
         q = [(root, 0)]
+        res = []
         
         while q:
             nxt = []
-            t = defaultdict(list)
-            for node, col in q:
-                t[col].append(node.val)
+            t = collections.defaultdict(list)
+            for node, i in q:
+                t[i].append(node.val)
                 if node.left:
-                    nxt.append((node.left, col - 1))
+                    nxt.append((node.left, i - 1))
                 if node.right:
-                    nxt.append((node.right, col + 1))
-            
+                    nxt.append((node.right, i + 1))
+                
+            q = nxt
             for k, v in t.items():
                 d[k] += sorted(v)
-            
-            q = nxt
-        
+                
         for k in sorted(d):
             res.append(d[k])
-        
+            
         return res
-        
-        
-        
-        
