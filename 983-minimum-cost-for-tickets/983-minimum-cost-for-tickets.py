d26e7e94eaa1a1ca48589e5d8d1cb93e874a9604
Dreamwings <huyb2009@gmail.com>
2022-09-25 00:26:44 -0700

    Time: 44 ms (95.66%), Space: 13.8 MB (91.63%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.py
@@ -1,25 +1,15 @@
-class Solution(object):
-    def mincostTickets(self, days, costs):
-        """
-        :type days: List[int]
-        :type costs: List[int]
-        :rtype: int
-        """
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         
-        M = days[-1]  # last day, also the largest num in days
-        dp = [0] * (M + 1)
+        n = days[-1]
+        dp = [0] * (n + 1)
+        d = set(days)
         
-        # for each day from day 1 to day M, check if it's in days
-        # if not, its dp[i] = dp[i-1]
-        # else it's the min of the 3 pass choices
-        days = set(days)
-        
-        for i in range(1, M+1):
-            if i not in days:
+        for i in range(1, n + 1):
+            if i not in d:
                 dp[i] = dp[i-1]
             else:
-                i1, i7, i30 = max(0, i - 1), max(0, i - 7), max(0, i - 30)
-                dp[i] = min(dp[i1] + costs[0],  dp[i7] + costs[1], dp[i30] + costs[2])
-                
-        return dp[-1]
+                i1, i7, i30 = max(0, i-1), max(0, i-7), max(0, i-30)
+                dp[i] = min(dp[i1] + costs[0], dp[i7] + costs[1], dp[i30] + costs[2])
         
+        return dp[-1]
