ac78027f3924625614379448c1241faa57137c28
Dreamwings <huyb2009@gmail.com>
2022-09-28 10:06:13 -0700

    Time: 262 ms (33.47%), Space: 14.6 MB (99.03%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -4,10 +4,11 @@ class Solution:
         from collections import Counter
         
         m, n = len(s), len(t)
-        if m < n or n == 0:
-            return ''
+        if m < n or n == 0: return ''
+        
         d = Counter(t)
-        l, r, i = 0, float('inf'), 0
+        l, r = 0, float('inf')
+        i = 0
         
         for j, c in enumerate(s):
             d[c] -= 1
@@ -18,6 +19,7 @@ class Solution:
                 while i < j and d[s[i]] < 0:
                     d[s[i]] += 1
                     i += 1
+                
                 if j - i < r - l:
                     l, r = i, j
         
