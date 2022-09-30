67f4bfcfbbc246aecbcaa4ba11e5112de65e72a5
Dreamwings <huyb2009@gmail.com>
2022-09-30 12:18:41 -0700

    Time: 45 ms (24.29%), Space: 13.6 MB (68.57%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.py
@@ -1,29 +1,32 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def subtreeWithAllDeepest(self, root: TreeNode) -> TreeNode:
+class Solution(object):
+    def subtreeWithAllDeepest(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
         
-        def dfs(node):
-            # return the LCA of all deepest nodes and the depth from the current node to the deepest nodes
-            # return (lca, depth)
-            
-            if not node:
+        ## S1: DFS
+        
+        def dfs(x):
+            if not x:
                 return None, 0
             
-            l, l_depth = dfs(node.left)
-            r, r_depth = dfs(node.right)
+            l, l_depth = dfs(x.left)
+            r, r_depth = dfs(x.right)
             
             if l_depth > r_depth:
                 return l, l_depth + 1
             elif l_depth < r_depth:
                 return r, r_depth + 1
             else:
-                return node, l_depth + 1
+                return x, l_depth + 1
         
         res, _ = dfs(root)
         return res
-        
+            
