07304e9d210e3a0d26c4a60f2213d212123fe978
Dreamwings <huyb2009@gmail.com>
2022-10-05 19:29:49 -0700

    Time: 357 ms (88.68%), Space: 13.6 MB (26.42%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -1,5 +1,11 @@
-class Solution:
-    def makesquare(self, matchsticks: List[int]) -> bool:
+class Solution(object):
+    def makesquare(self, matchsticks):
+        """
+        :type matchsticks: List[int]
+        :rtype: bool
+        """
+        
+        ## S2: DFS Backtracking
         
         s = sum(matchsticks)
         if s % 4:
@@ -32,6 +38,38 @@ class Solution:
         
         return dfs(0)
                 
-                
-                
         
+        """
+        ## S1: Time Limit Exceeded
+        
+        s = sum(matchsticks)
+        if s % 4: 
+            return False
+        s, n = s // 4, len(matchsticks)
+        
+        a = sorted(matchsticks, reverse=True)
+        if a[0] > s:
+            return False
+        
+        def dfs(i, s1, s2, s3, s4):
+            if i == n:
+                return s1 == s2 == s3 == s4 == s
+                
+            if s1 > s or s2 > s or s3 > s or s4 > s:
+                return False
+            
+            res = False
+            for k in range(4):
+                if k == 0:
+                    res = res or dfs(i+1, s1 + a[i], s2, s3, s4)
+                if k == 1:
+                    res = res or dfs(i+1, s1, s2 + a[i], s3, s4)
+                if k == 2:
+                    res = res or dfs(i+1, s1, s2, s3 + a[i], s4)
+                if k == 3:
+                    res = res or dfs(i+1, s1, s2, s3, s4 + a[i])
+            
+            return res
+        
+        return dfs(0, 0, 0, 0, 0)
+        """ 
