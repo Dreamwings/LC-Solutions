94fd1e2a73bdeb16a8d6a1256400dc5981669ac7
Dreamwings <huyb2009@gmail.com>
2022-10-08 15:18:13 -0700

    Time: 30 ms (96.99%), Space: 14 MB (16.27%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -6,8 +6,10 @@ class Solution:
         for i in range(1, int(n**0.5) + 1):
             if n % i == 0:
                 a.append(i)
-                if i * i != n:
-                    b.append(n // i)
+                b.append(n // i)
+        
+        if a[-1] == b[-1]: # sqrt of n
+            b.pop()
             
         la, lb = len(a), len(b)
         if k > la + lb:
