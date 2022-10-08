8072f04713f8c78f77a9e9b63b58bde1abcc3725
Dreamwings <huyb2009@gmail.com>
2022-10-08 15:30:22 -0700

    Time: 5995 ms (44.21%), Space: 33.6 MB (61.58%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,10 +1,19 @@
-class Solution:
-    def countSmaller(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        ## S1: Bisect
+        ## Time: O(NlogN)
+        ## https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/solution/bu-dao-10xing-dai-ma-zui-jian-dan-fang-fa-mei-you-/
         
         from bisect import bisect_left, insort
         
-        a, res = [], []
-        # a is to store the sorted elements from the right of nums
+        res = []
+        a = []
+        # a is the sorted array
         
         for x in nums[::-1]:
             i = bisect_left(a, x) # this is the number of smaller elements to the right of x
