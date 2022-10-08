4f3a3166b4650695ced52ca9df39d5b15a85e6f3
Dreamwings <huyb2009@gmail.com>
2022-10-08 00:45:52 -0700

    Time: 333 ms (69.00%), Space: 14.9 MB (74.91%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -3,6 +3,23 @@ class Solution:
         
         from collections import Counter
         
+        ## S2: Optimization of S1
+        
+        res = flips = s.count('0')
+        
+        for c in s:
+            if c == '1':
+                flips += 1
+            else:
+                flips -= 1
+                res = min(res, flips)
+        
+        return res
+        
+        """
+        ## S1:
+        ## https://leetcode.com/problems/flip-string-to-monotone-increasing/discuss/184110/python-O(n)-time-O(1)-space-solution-with-explanation(with-extra-Chinese-explanation)
+        
         d = Counter(s)
         zero_left = d['0']  # how many '0' left on the right of the current place
         ones_seen = 0       # how many '1' counted by the current place
@@ -15,4 +32,5 @@ class Solution:
                 res = min(res, zero_left + ones_seen)
                 ones_seen += 1
                 
-        return res        
+        return res   
+        """
