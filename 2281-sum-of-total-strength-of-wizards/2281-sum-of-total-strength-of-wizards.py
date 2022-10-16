7d53b8e21fc009d25c29943d293342e1e39fd0e0
Dreamwings <huyb2009@gmail.com>
2022-10-16 02:56:16 -0700

    Time: 2010 ms (61.54%), Space: 27.8 MB (73.63%) - LeetHub

--- 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.py
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.py
@@ -1,7 +1,50 @@
-class Solution:
-    def totalStrength(self, nums: List[int]) -> int:
+class Solution(object):
+    def totalStrength(self, nums):
+        """
+        :type strength: List[int]
+        :rtype: int
+        """
+        ## S1: 
+        ## https://leetcode.com/problems/sum-of-total-strength-of-wizards/discuss/2061985/JavaC%2B%2BPython-One-Pass-Solution
+        ## Time: O(N)
+        ## Space: O(N)
+        
+        n = len(nums)
+        M = 10 ** 9 + 7
+        if n == 1:
+            return nums[0] * nums[0] % M
+        
+        s = 0 # prefix sum
+        stack = [-1] # to store the index of min val for subarray ended at current val
+        acc = [0] # to store sum of presum
+        nums += [0]
+        res = 0
+        # l [............ i ............] r
+        # l: last min index
+        # i: current min index
+        # r: next min index
+        
+        for r, x in enumerate(nums):
+            s += x
+            acc.append(s + acc[-1])
+            while stack and nums[stack[-1]] > x:
+                i = stack.pop()
+                l = stack[-1]
+                lacc = acc[i] - acc[max(0, l)]  # sum of presum between l and i
+                racc = acc[r] - acc[i]          # sum of presum between i and r
+                # for ']' at any place between i and r, it always count nums[i] * lacc
+                # for '[' at any place between l and i, it always count nums[i] * racc
+                res += nums[i] * (racc * (i - l) - lacc * (r - i))
+                res %= M
+                
+            stack.append(r)
+            
+        return res % M
+        
+        """
+                
         ## S2:
-        ## https://leetcode.cn/problems/sum-of-total-strength-of-wizards/solution/dan-diao-zhan-qian-zhui-he-de-qian-zhui-d9nki/
+        ## 
         ## Time: O(N)
         ## Space: O(N)
         
@@ -36,5 +79,4 @@ class Solution:
         return res % M
         
         
-        
-        
+        """
