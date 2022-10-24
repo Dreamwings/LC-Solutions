bb475fec08dcc084f87c9050f277a49619b11104
Dreamwings <huyb2009@gmail.com>
2022-10-24 00:08:29 -0700

    Time: 670 ms (31.00%), Space: 34.4 MB (5.00%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -8,9 +8,9 @@ class Solution(object):
         :rtype: int
         """
         
-        h = sorted([0] + horizontalCuts + [h])
-        w = sorted([0] + verticalCuts + [w])
-        x = max(b - a for a, b in zip(h, h[1:]))
-        y = max(b - a for a, b in zip(w, w[1:]))
+        ha = sorted([0] + horizontalCuts + [h])
+        wa = sorted([0] + verticalCuts + [w])
+        x = max(b - a for a, b in zip(ha, ha[1:]))
+        y = max(b - a for a, b in zip(wa, wa[1:]))
         return x * y % int(1e9+7)
         
