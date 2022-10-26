6086bb1b79d1d2884057b6d007039bbb986e055b
Dreamwings <huyb2009@gmail.com>
2022-10-26 16:52:19 -0700

    Time: 1133 ms (89.81%), Space: 25.1 MB (38.47%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         
         m, res = prices[0], 0
         
         for x in prices:
+            # better than prices[1:] because list slice takes time O(k)
             m = min(m, x)
             res = max(res, x - m)
         
